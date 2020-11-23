0143b1c5557cd31f6bbd39d8d66c3c0e4c51060f
CharlesHaden <68151344+CharlesHaden@users.noreply.github.com>
2020-11-23 16:49:08 +0000

    added how has your day been

--- mainMenu.java
+++ mainMenu.java
@@ -2,5 +2,6 @@ public class mainMenu {
     public static void main(String[] args){
         
         System.out.println("Hello world");
+        System.out.println("how has your day been?");
     }
 }
