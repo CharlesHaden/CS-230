4f75f08cdae72cc2203f2bb5be16181532f9d548
nbman07 <74788443+nbman07@users.noreply.github.com>
2020-11-25 18:36:37 +0000

    Add files via upload

---
+++ Backtrack.java
@@ -0,0 +1,14 @@
+/** 
+ * 
+ * 
+ * @author Nim Man
+ * @author Hyder Al-Hashimi
+*/
+public class Backtrack extends actionTile {
+    public void action() {
+    }  
+
+    public void isPlayable() {
+        return true;
+    }
+}
---
+++ actionTile.java
@@ -0,0 +1,26 @@
+/** 
+ * 
+ * 
+ * @author Nim Man
+ * @author Hyder Al-Hashimi
+*/
+
+abstract class actionTile extends Tile {
+    public abstract void action();
+    public abstract void isPlayable();
+
+    public String getTileType() {
+        return "Action Tile";
+
+    }
+
+}
+
+
+ 
+
+
+    
+
+    
+        
