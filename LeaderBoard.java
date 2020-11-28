5e6cdac3bccd5ab676a9292b46a0b292ab81e144
MatthewGClarke <74783734+MatthewGClarke@users.noreply.github.com>
2020-11-28 15:27:49 +0000

    Update LeaderBoard.java
    added a bubble sort (as the profile array is likely to be relatively small) which will change the order depending on the boolean ascending

--- LeaderBoard.java
+++ LeaderBoard.java
@@ -7,15 +7,18 @@
 public class LeaderBoard {
 	
 	//Used to store which order to display the profiles. 
-	private String[] profileList;
+	private Profile[] profileList;
 	//Used to store which order to display the profiles.  
-	private boolean ACSENDING = true;
+	private boolean ascending = true;
+	//Used in the bubble sort
+	private boolean sorted;
+	private Profile temp;
 	
 	/**
 	 * Constructs a LeaderBoard object and does't take any parameters.
 	 */
-	public LeaderBoard() {
-		
+	public LeaderBoard(Profile[] profileList) {
+		this.profileList = profileList;
 	}
 	
 	/**
@@ -23,7 +26,7 @@ public class LeaderBoard {
 	 * 
 	 * @return Return type is an array of strings and is the profileList.
 	 */
-    public String[] getProfileList() {
+    public Profile[] getProfileList() {
         return profileList;
     }
     
@@ -32,17 +35,31 @@ public class LeaderBoard {
 	 * 
 	 * @param profileList Stores the array of strings used to store the profile classes
 	 */
-    public void setProfileList(String[] profileList) {
+    public void setProfileList(Profile[] profileList) {
         this.profileList = profileList;
     }
     
 	/**
-	 * Prints all profiles in the current array/list with their associated wins and losses.
+	 * Returns all profiles in the current array/list with their associated wins and losses.
 	 * 
+	 * Side effects: changes stored profileList in LeaderBoard as opposed to just returning it
 	 * @return Returns the sorted profile list in either ascending or descending order.
 	 */
-	public String[] sortBy() {
+	public Profile[] sortBy() {
+		// Bubble sort as array is likely to be small
+		sorted = false;
+		while(!sorted) {
+			sorted = true;
+			for (int i = 0; i < profileList.length - 1; i++) {
+				if ((profileList[i].getWins() > profileList[i+1].getWins()) ^ !ascending) {
+					temp = profileList[i];
+					profileList[i] = profileList[i+1];
+					profileList[i+1] = temp;
+					sorted = false;
+				}
+			}
+		}
 		return profileList;
 	}
-	
+
 }
